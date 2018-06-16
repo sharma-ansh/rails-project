968f82affd115d80191b4c503b6313ef60f911f9
sharma-ansh <sharmaanshul670@gmail.com>
2018-06-17 00:26:02 +0530

    change index to show current user docs

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -2,7 +2,7 @@ class DocsController < ApplicationController
   before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
   def index
-    @docs = Doc.all
+    @docs = current_user.docs
   end
 
   def show
